478536e7f5516ac120ae4069f7fc472bb78d4654
divyansh <dpi0.dev@proton.me>
2025-11-29 15:49:44 +0530

    Add a visible area ratio to selection

--- screenshot/area.sh
+++ screenshot/area.sh
@@ -1,28 +1,19 @@
 #!/usr/bin/env bash
 
-# SCREENSHOT_DIR="$HOME/Screenshots"
-# SCRIPT_PATH="$HOME/scripts"
-# FILENAME="A_($(date +'%H-%M-%S'))_($(date +'%d-%b-%y')).png"
-
-# mkdir -p "$SCREENSHOT_DIR"
-
-# hyprpicker -r -z &
-# sleep 0.1
-# "$SCRIPT_PATH/hyprshot.sh" \
-#   -m region \
-#   -o "$SCREENSHOT_DIR" \
-#   -f "$FILENAME"
-
 GRIMBLAST="$HOME/bin/contrib/grimblast/grimblast"
 DIR="$HOME/Screenshots"
 mkdir -p "$DIR"
 
 timestamp="$(date +'%H-%M-%S')"
 datestamp="$(date +'%d-%b-%y')"
+
 window_name=$(hyprctl activewindow -j | jq -r '.title' 2>/dev/null)
 [ -z "$window_name" ] && window_name="NoWindow"
-window_name=$(echo "$window_name" | tr -d '"' | tr -c '[:alnum:]\ \-_' '_')
+window_name=$(echo "$window_name" | tr -d '"' | tr -c '[:alnum:]\ \-_' '_' | sed 's/__*/_/g')
 
 filename="$DIR/A $timestamp $datestamp - $window_name.png"
+
+export SLURP_ARGS="-d"
+
 "$GRIMBLAST" --freeze copysave area "$filename"
 [ -f "$filename" ] && dunstify -i "$filename" "Screenshot Saved" "$(basename "$filename")"
