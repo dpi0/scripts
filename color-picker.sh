4da57d00df6d05e749a497d02a9becaa9b8236d4
divyansh <dpi0.dev@proton.me>
2025-11-29 15:45:39 +0530

    Add more color formats to picker

--- color-picker.sh
+++ color-picker.sh
@@ -1,12 +1,25 @@
 #!/usr/bin/env bash
 
-color=$(hyprpicker --autocopy --no-fancy --format=hex)
+hex=$(hyprpicker --autocopy --no-fancy --format=hex)
 
-if [ -n "$color" ]; then
+# [possible values: rgb, rgb-float, rgb-r, rgb-g, rgb-b, hex, hsl, hsl-hue, hsl-saturation,
+# hsl-lightness, hsv, hsv-hue, hsv-saturation, hsv-value, lch, lch-lightness, lch-chroma,
+# lch-hue, oklch, oklch-lightness, oklch-chroma, oklch-hue, lab, lab-a, lab-b, oklab,
+# oklab-l, oklab-a, oklab-b, luminance, brightness, ansi-8bit, ansi-24bit, ansi-8bit-value,
+# ansi-8bit-escapecode, ansi-24bit-escapecode, cmyk, name]
+
+if [ -n "$hex" ]; then
+	hsl=$(pastel format hsl "$hex")
+	rgb=$(pastel format rgb "$hex")
+	oklch=$(pastel format oklch "$hex")
+
+	fg="$(pastel textcolor "$hex" | pastel format hex)"
+
 	notify-send \
-		-h "string:fgcolor:$color" \
-		-h "string:frcolor:$color" \
-		"Color: $color"
+		-h "string:fgcolor:$hex" \
+		-h "string:frcolor:$hex" \
+		-h "string:bgcolor:$fg" \
+		"Color Picked" "Hex: $hex\nHSL: $hsl\nRGB: $rgb\nOKLCh: $oklch"
 else
 	notify-send "Color Picker" "No color selected"
 fi
