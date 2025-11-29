9533056e0e3e056fe408c6fcb864cc78abcbfc1b
divyansh <dpi0.dev@proton.me>
2025-11-29 15:50:02 +0530

    Add active window screenshot

---
+++ screenshot/active-window.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+GRIMBLAST="$HOME/bin/contrib/grimblast/grimblast"
+DIR="$HOME/Screenshots"
+mkdir -p "$DIR"
+
+timestamp="$(date +'%H-%M-%S')"
+datestamp="$(date +'%d-%b-%y')"
+
+window_name=$(hyprctl activewindow -j | jq -r '.title' 2>/dev/null)
+[ -z "$window_name" ] && window_name="NoWindow"
+window_name=$(echo "$window_name" | tr -d '"' | tr -c '[:alnum:]\ \-_' '_' | sed 's/__*/_/g')
+
+filename="$DIR/W $timestamp $datestamp - $window_name.png"
+
+export SLURP_ARGS="-d"
+
+"$GRIMBLAST" --freeze copysave active "$filename"
+[ -f "$filename" ] && dunstify -i "$filename" "Screenshot Saved" "$(basename "$filename")"
