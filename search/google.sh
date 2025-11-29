4e26d1ec262b0a6f367278fea6fa5e90031d3817
divyansh <dpi0.dev@proton.me>
2025-11-29 22:42:31 +0530

    Update google search url to a cleaner one

--- search/google.sh
+++ search/google.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 clipboard_text=$(/usr/bin/wl-paste)
-xdg-open "http://www.google.com/search?q=$clipboard_text"
+xdg-open "https://www.google.com/search?udm=14&q=$clipboard_text&num=50"
 hyprctl dispatch workspace 1
