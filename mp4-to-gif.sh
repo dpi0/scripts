b4fa1fe94be02093bf34b9ff5fee02eb3e05b2bd
divyansh <dpi0.dev@proton.me>
2025-11-29 15:50:54 +0530

    Add mp4 to gif ffmpeg decent quality conversion for memes

---
+++ mp4-to-gif.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bas
+
+# https://blog.pkh.me/p/21-high-quality-gif-with-ffmpeg.html
+
+in="$1"
+out="${2:-${in%.*}.gif}"
+palette="$(mktemp --suffix=.png)"
+fps=30
+width=240
+filters="fps=${fps},scale=${width}:-1:flags=lanczos"
+
+ffmpeg -v warning -i "$in" \
+  -vf "${filters},palettegen" \
+  -frames:v 1 \
+  -y "$palette"
+
+ffmpeg -v warning -i "$in" -i "$palette" \
+  -lavfi "${filters} [x]; [x][1:v] paletteuse" \
+  -y "$out"
+
+rm -f "$palette"
