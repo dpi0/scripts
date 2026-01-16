64c990213752cd85cf90f0013ded6c5ee3e83b2b
divyansh <dpi0.dev@proton.me>
2026-01-16 15:00:49 +0530

    Add sensible include dirs in file finder

--- rofi/file-finder.sh
+++ rofi/file-finder.sh
@@ -2,23 +2,21 @@
 
 INCLUDE_DIRS=(
   "$HOME/Backup"
-  "$HOME/bin"
-  "$HOME/Books"
   "$HOME/Downloads"
   "$HOME/Pictures"
   "$HOME/projects"
   "$HOME/Screenshots"
   "$HOME/scripts"
   "$HOME/sh"
-  "$HOME/temp"
   "$HOME/Videos"
   "$HOME/Wallpapers"
-  "$HOME/.dotfiles"
+  "$HOME/notes"
 )
 
 selection=$(
   fd . "${INCLUDE_DIRS[@]}" \
     --type f \
+    --exclude .git \
     --hidden 2>/dev/null |
     sed "s|$HOME|~|" |
     rofi -sort -sorting-method fzf -disable-history -dmenu -no-custom -p "file: " |
@@ -47,54 +45,3 @@ txt | py | rs | go | md | c | cpp | h | java | sh | ts | js | json | toml | yaml
   exec kitty nvim "$selection"
   ;;
 esac
-
-# EXCLUDE LIST
-# selection=$(
-#   fd . "$HOME" \
-#     --type f \
-#     --hidden \
-#     --exclude node_modules \
-#     --exclude go/pkg/mod \
-#     --exclude .local/share \
-#     --exclude .local/state \
-#     --exclude .vscode \
-#     --exclude .config/Code \
-#     --exclude .Trash-1000 \
-#     --exclude .git \
-#     --exclude .cargo \
-#     --exclude .rustup \
-#     --exclude .cache \
-#     --exclude .mozilla \
-#     --exclude .npm \
-#     --exclude .bun \
-#     --exclude .terrascan \
-#     --exclude .bundle \
-#     --exclude .gradle \
-#     --exclude .pub-cache \
-#     --exclude .config/libreoffice/ \
-#     --exclude .flutter-devtools \
-#     --exclude .claude \
-#     --exclude .dart-tool \
-#     --exclude .dotnet \
-#     --exclude .opencode \
-#     --exclude Android \
-#     --exclude .kube \
-#     --exclude .keychain \
-#     --exclude .dartServer \
-#     --exclude .java \
-#     --exclude .password-store \
-#     --exclude .android \
-#     --exclude .gemini \
-#     --exclude .vagrant.d \
-#     --exclude .config \
-#     --exclude .zen \
-#     --exclude .tmux \
-#     --exclude .ansible \
-#     --exclude .docker \
-#     --exclude .gnupg \
-#     --exclude .logseq \
-#     2>/dev/null |
-#     sed "s|$HOME|~|" |
-#     rofi -sort -sorting-method fzf -disable-history -dmenu -no-custom -p "file: " |
-#     sed "s|~|$HOME|"
-# )
